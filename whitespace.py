239a645f6446b7a41f11289d73dce8397240f91a
Nick Stielau <nick.stielau@gmail.com>
2011-11-22 13:23:23 -0800

    Adding helpful whitespace plugin.

---
+++ whitespace.py
@@ -0,0 +1,36 @@
+import sublime, sublime_plugin
+
+class ExpandTabs(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if view.settings().get("expand_tabs") == True:
+            view.run_command('expand_tabs')
+
+class TrimTrailingWhiteSpace(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if view.settings().get("trim_trailing_white_space_on_save") == True:
+            trailing_white_space = view.find_all("[\t ]+$")
+            trailing_white_space.reverse()
+            edit = view.begin_edit()
+            for r in trailing_white_space:
+                view.erase(edit, r)
+            view.end_edit(edit)
+
+class EnsureNewlineAtEof(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if view.settings().get("ensure_newline_at_eof_on_save") == True:
+            i = 0
+            edit = view.begin_edit()
+            while i <= 20 and view.size() > 0 and view.substr(view.full_line(view.size() - 1)) == '\n':
+                i = i + 1
+                # print "%d: Last line is a newline, removing" % i
+                view.erase(edit, view.full_line(int(view.size() - 1)))
+
+            # Only apply last 20 edits
+            if i < 20:
+                view.end_edit(edit)
+            # Add a newline if necesary
+            if view.size() > 0 and view.substr(view.size() - 1) != '\n':
+                # print "Last line is NOT a newline, adding..."
+                edit = view.begin_edit()
+                view.insert(edit, view.size(), "\n")
+                view.end_edit(edit)
