4f73dc09b8857ca4bfdb2fb9613224ef7ea5898d
Nick Stielau <nick.stielau@gmail.com>
2011-12-22 09:12:24 -0800

    Cleaning up whitespace plugin.

--- whitespace.py
+++ whitespace.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin
 
 class ExpandTabs(sublime_plugin.EventListener):
     def on_pre_save(self, view):
-        if view.settings().get("expand_tabs") == True:
+        if view.settings().get("expand_tabs_on_save") == True:
             view.run_command('expand_tabs')
 
 class TrimTrailingWhiteSpace(sublime_plugin.EventListener):
@@ -18,15 +18,17 @@ class TrimTrailingWhiteSpace(sublime_plugin.EventListener):
 class EnsureNewlineAtEof(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         if view.settings().get("ensure_newline_at_eof_on_save") == True:
+            # Remove up to 'max_lines' empty lines on save
+            max_lines = 20
             i = 0
             edit = view.begin_edit()
-            while i <= 20 and view.size() > 0 and view.substr(view.full_line(view.size() - 1)) == '\n':
+            while i <= max_lines and view.size() > 0 and view.substr(view.full_line(view.size() - 1)) == '\n':
                 i = i + 1
                 # print "%d: Last line is a newline, removing" % i
                 view.erase(edit, view.full_line(int(view.size() - 1)))
 
-            # Only apply last 20 edits
-            if i < 20:
+            # Only apply last 'max_lines' edits
+            if i < max_lines:
                 view.end_edit(edit)
             # Add a newline if necesary
             if view.size() > 0 and view.substr(view.size() - 1) != '\n':
