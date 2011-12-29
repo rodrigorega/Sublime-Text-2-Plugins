96f688e0c21cba4cc8389b43fc7f628d88c168ec
Nick Stielau <nick.stielau@gmail.com>
2011-12-28 17:19:05 -0800

    Improving status message.

--- pep8check.py
+++ pep8check.py
@@ -16,8 +16,8 @@ class Pep8CheckCommand(sublime_plugin.TextCommand):
     The pep8 executable must be in your system's path for this to work.
     """
 
-    def goto_line(self, index):
-        # TODO: Use goto line Command, highline line...
+    def goto_error(self, index):
+        # TODO: Highline line...
 
         if index >= 0:
             line = int(self.index_to_line[index]) - 1
@@ -31,23 +31,27 @@ class Pep8CheckCommand(sublime_plugin.TextCommand):
 
             self.view.show(pt)
         else:
-            sublime.status_message("PEP8 Valid!")
+            if len(self.errors) == 0:
+                sublime.status_message("PEP8 Valid!")
 
     def run(self, edit):
         if self.view.file_name().endswith('.py'):
             sublime.status_message("Running PEP8 " + self.view.file_name())
-            output = Popen(["/usr/local/bin/pep8", self.view.file_name()],
-                                                stdout=PIPE).communicate()[0]
+            output = Popen(["/usr/local/bin/pep8",
+                            self.view.file_name(),
+                            "--repeat"], stdout=PIPE).communicate()[0]
             self.errors = []
             self.index_to_line = {}
             pep8_regex = re.compile('(.*):(.*):(.*): (....) (.*)')
             for line in output.split('\n'):
+                print line
                 if len(line) > 0:
                     _, row, col, err, msg = re.match(pep8_regex, line).groups()
                     self.index_to_line[len(self.errors)] = row
                     self.errors.append("Line %s: %s %s" % (row, err, msg))
 
-            self.view.window().show_quick_panel(self.errors, self.goto_line)
+            self.view.window().show_quick_panel(self.errors, self.goto_error)
+            sublime.status_message("%s PEP8 errors " % len(self.errors))
 
     def is_enabled(self):
         return self.view.file_name() and self.view.file_name().endswith('.py')
