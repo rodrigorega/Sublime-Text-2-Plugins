4992c76571073daa1645c8b90144a620e908d59b
Nick Stielau <nick.stielau@gmail.com>
2011-12-31 14:36:29 -0800

    Adding support for pylint.

--- pep8check.py
+++ pep8check.py
@@ -1,4 +1,3 @@
-import os
 import re
 from subprocess import Popen
 from subprocess import PIPE
@@ -7,14 +6,11 @@ import sublime
 import sublime_plugin
 
 
-class Pep8CheckCommand(sublime_plugin.TextCommand):
-
-    """ This will invoke PEP8 checking on the given file and
-    provide a pop-up with validation errors that will jump to
-    the line-in-question.
-
-    The pep8 executable must be in your system's path for this to work.
-    """
+class PythonCheckCommand(sublime_plugin.TextCommand):
+    def __init__(self, *args):
+        sublime_plugin.TextCommand.__init__(self, *args)
+        self.errors = []
+        self.index_to_line = {}
 
     def goto_error(self, index):
         # TODO: Highlight line...
@@ -35,17 +31,26 @@ class Pep8CheckCommand(sublime_plugin.TextCommand):
             if len(self.errors) == 0:
                 sublime.status_message("PEP8 Valid!")
 
+    def is_enabled(self):
+        return self.view.file_name() and self.view.file_name().endswith('.py')
+
+
+class Pep8CheckCommand(PythonCheckCommand):
+    """ This will invoke PEP8 checking on the given file and
+    provide a pop-up with validation errors that will jump to
+    the line-in-question.
+
+    The pep8 executable must be in your system's path for this to work.
+    """
     def run(self, edit):
+        self.errors = []
         if self.view.file_name().endswith('.py'):
             sublime.status_message("Running PEP8 " + self.view.file_name())
             output = Popen(["/usr/local/bin/pep8",
                             self.view.file_name(),
                             "--repeat"], stdout=PIPE).communicate()[0]
-            self.errors = []
-            self.index_to_line = {}
             pep8_regex = re.compile('(.*):(.*):(.*): (....) (.*)')
             for line in output.split('\n'):
-                print line
                 if len(line) > 0:
                     _, row, col, err, msg = re.match(pep8_regex, line).groups()
                     self.index_to_line[len(self.errors)] = row
@@ -54,5 +59,27 @@ class Pep8CheckCommand(sublime_plugin.TextCommand):
             self.view.window().show_quick_panel(self.errors, self.goto_error)
             sublime.status_message("%s PEP8 errors " % len(self.errors))
 
-    def is_enabled(self):
-        return self.view.file_name() and self.view.file_name().endswith('.py')
+
+class PylintCheckCommand(PythonCheckCommand):
+    """ This will invoke pylint validation on the given file and provide
+    a pop-up with validation errors that will jump to the line-in-question.
+
+    The pylint executable must be in your system's path fro this to work.
+    """
+    def run(self, edit):
+        self.errors = []
+        if self.view.file_name().endswith('.py'):
+            sublime.status_message("Running pylint " + self.view.file_name())
+            output = Popen(['/usr/local/bin/pylint',
+                            self.view.file_name()],
+                           stdout=PIPE).communicate()[0]
+            pylint_regex = re.compile('^([A-Z]): (\d+),(\d+):(.+):\s+(.+)')
+            for line in output.split('\n'):
+                re_result = re.match(pylint_regex, line)
+                if re_result:
+                    err, row, col, __, msg = re_result.groups()
+                    self.index_to_line[len(self.errors)] = row
+                    self.errors.append('Line %s: (%s) %s' % (row, err, msg))
+
+            self.view.window().show_quick_panel(self.errors, self.goto_error)
+            sublime.status_message("%s Pylint errors " % len(self.errors))
