626e88b7856b5a0e64ae527f95def166235dbe55
Nick Stielau <nick.stielau@gmail.com>
2012-01-02 11:58:23 -0800

    Cleaning up whitespace management and python validations.

--- python_validation.py
+++ python_validation.py
@@ -1,16 +1,45 @@
+import os.path
 import re
-from subprocess import Popen
-from subprocess import PIPE
+import subprocess
 
 import sublime
 import sublime_plugin
 
 
-class PythonCheckCommand(sublime_plugin.TextCommand):
+def find_binary(name):
+    dirs = ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin',
+            '/usr/bin', '/sbin', '/bin']
+    for directory in dirs:
+        path = os.path.join(directory, name)
+        if os.path.exists(path):
+            return path
+
+    raise Exception('The binary %s could not be located' % name)
+
+
+class PythonValidateCommand(sublime_plugin.TextCommand):
     def __init__(self, *args):
         sublime_plugin.TextCommand.__init__(self, *args)
         self.errors = []
         self.index_to_line = {}
+
+    def validate(self):
+        """Subclass to add errors."""
+        pass
+
+    def validator(self):
+        """Subclass to specify validator."""
+        pass
+
+    def run(self, edit):
+        self.errors = []
+        if self.view.file_name().endswith('.py'):
+            sublime.status_message("Running %s on %s" % (self.view.file_name(),
+                                                         self.validator()))
+            self.validate()
+            sublime.status_message("%s %s errors " % (len(self.errors),
+                                                      self.validator()))
+            self.view.window().show_quick_panel(self.errors, self.goto_error)
 
     def goto_error(self, index):
         # TODO: Highlight line...
@@ -19,7 +48,7 @@ class PythonCheckCommand(sublime_plugin.TextCommand):
         if index >= 0:
             line = int(self.index_to_line[index]) - 1
 
-            sublime.status_message("PEP8: %s" % self.errors[index])
+            sublime.status_message("Validation Error: %s" % self.errors[index])
 
             pt = self.view.text_point(line, 0)
 
@@ -29,57 +58,66 @@ class PythonCheckCommand(sublime_plugin.TextCommand):
             self.view.show(pt)
         else:
             if len(self.errors) == 0:
-                sublime.status_message("PEP8 Valid!")
+                sublime.status_message("No validation errors!")
+
+    def execute(self, args=None):
+        if not args:
+            args = []
+        args.insert(0, self.view.file_name())
+        args.insert(0, find_binary(self.validator()))
+        proc = subprocess.Popen(args, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        output = proc.stdout.read()
+        return output
+
+    def add_error(self, row, msg, err):
+        """Adds a validation error"""
+        ignored_ids_key = '%s_ignored_ids' % self.validator()
+        ignored_ids = []  # self.view.settings().get(ignored_ids_key, [])
+        if err not in ignored_ids:
+            self.index_to_line[len(self.errors)] = row
+            self.errors.append('Line %s: %s' % (row, msg))
 
     def is_enabled(self):
+        """Enable for Python files."""
         return self.view.file_name() and self.view.file_name().endswith('.py')
 
 
-class Pep8CheckCommand(PythonCheckCommand):
+class Pep8ValidateCommand(PythonValidateCommand):
     """ This will invoke PEP8 checking on the given file and
     provide a pop-up with validation errors that will jump to
     the line-in-question.
 
     The pep8 executable must be in your system's path for this to work.
     """
-    def run(self, edit):
-        self.errors = []
-        if self.view.file_name().endswith('.py'):
-            sublime.status_message("Running PEP8 " + self.view.file_name())
-            output = Popen(["/usr/local/bin/pep8",
-                            self.view.file_name(),
-                            "--repeat"], stdout=PIPE).communicate()[0]
-            pep8_regex = re.compile('(.*):(.*):(.*): (....) (.*)')
-            for line in output.split('\n'):
-                if len(line) > 0:
-                    _, row, col, err, msg = re.match(pep8_regex, line).groups()
-                    self.index_to_line[len(self.errors)] = row
-                    self.errors.append("Line %s: %s %s" % (row, err, msg))
+    def validator(self):
+        return "pep8"
 
-            self.view.window().show_quick_panel(self.errors, self.goto_error)
-            sublime.status_message("%s PEP8 errors " % len(self.errors))
+    def validate(self):
+        output = self.execute(['--repeat'])
+        pep8_regex = re.compile('(.*):(.*):(.*): (....) (.*)')
+        for line in output.split('\n'):
+            re_result = re.match(pep8_regex, line)
+            if re_result:
+                _, row, col, err_id, msg = re_result.groups()
+                self.add_error(row, "%s %s" % (err_id, msg), err_id)
 
 
-class PylintCheckCommand(PythonCheckCommand):
+class PylintValidateCommand(PythonValidateCommand):
     """ This will invoke pylint validation on the given file and provide
     a pop-up with validation errors that will jump to the line-in-question.
 
     The pylint executable must be in your system's path fro this to work.
     """
-    def run(self, edit):
-        self.errors = []
-        if self.view.file_name().endswith('.py'):
-            sublime.status_message("Running pylint " + self.view.file_name())
-            output = Popen(['/usr/local/bin/pylint',
-                            self.view.file_name()],
-                           stdout=PIPE).communicate()[0]
-            pylint_regex = re.compile('^([A-Z]): (\d+),(\d+):(.+):\s+(.+)')
-            for line in output.split('\n'):
-                re_result = re.match(pylint_regex, line)
-                if re_result:
-                    err, row, col, __, msg = re_result.groups()
-                    self.index_to_line[len(self.errors)] = row
-                    self.errors.append('Line %s: (%s) %s' % (row, err, msg))
+    def validator(self):
+        return "pylint"
 
-            self.view.window().show_quick_panel(self.errors, self.goto_error)
-            sublime.status_message("%s Pylint errors " % len(self.errors))
+    def validate(self):
+        output = self.execute(['--reports=no', '--include-ids=yes'])
+        pylint_regex = re.compile('^(.*): (\d+),(\d+):(.+):\s+(.+)')
+        for line in output.split('\n'):
+            re_result = re.match(pylint_regex, line)
+            if re_result:
+                err_id, row, col, __, msg = re_result.groups()
+                self.add_error(row, '(%s) %s' % (err_id, msg), err_id)
--- whitespace.py
+++ whitespace.py
@@ -4,6 +4,7 @@ import sublime_plugin
 
 
 class WhitespaceListener(sublime_plugin.EventListener):
+    """Event listener to manage whitespace on save"""
     def on_pre_save(self, view):
         if view.settings().get('expand_tabs_on_save') == True:
             view.run_command('expand_tabs')
@@ -11,27 +12,40 @@ class WhitespaceListener(sublime_plugin.EventListener):
             view.run_command('trim_trailing_whitespace')
         if view.settings().get('ensure_newline_at_eof_on_save') == True:
             view.run_command('ensure_newline_at_eof')
-        # view.run_command('pep8_check')
 
 
 class TrimTrailingWhitespace(sublime_plugin.TextCommand):
-    def run(self, edit):
-        trailing_white_space = self.view.find_all("[\t ]+$").reverse()
+    """Trims trailing whitespace."""
+    def run(self, *args):
+        trailing_white_space = self.view.find_all("[\t ]+$")
+        edit = self.view.begin_edit()
         for match in trailing_white_space:
             self.view.erase(edit, match)
         self.view.end_edit(edit)
 
 
 class EnsureNewlineAtEof(sublime_plugin.TextCommand):
-    def run(self, edit):
+    """Ensures the desired number of trailing new lines.
+
+    Set 'max_newline_removals' to the number of newlines to be removed ('
+    a performance/sanity check.')
+
+    Set 'trailing_newlines' to the number of desired newlines at the end of
+    the file.
+    """
+    def run(self, *args):
+        # Remove up to *max_newline_removals* trailing new lines.
         max_removals = self.view.settings().get('max_newline_removals', 20)
+        edit = self.view.begin_edit()
         for i in range(max_removals):
             size = self.view.size()
             last_line = self.view.full_line(size - 1)
             if size > 0 and self.view.substr(last_line) == '\n':
                 self.view.erase(edit, last_line)
 
-        if not self.view.substr(last_line).endswith('\n'):
-            self.view.insert(edit, self.view.size(), '\n')
-
+        # Add desired trailing newlines
+        trailing_newlines = self.view.settings().get('trailing_newlines', 1)
+        for i in range(trailing_newlines):
+            if not self.view.substr(last_line).endswith('\n'):
+                self.view.insert(edit, self.view.size(), '\n')
         self.view.end_edit(edit)
